577f5256cc1c44b91d311bfd0f415497b287539a
EvgeniLen <evgenilen@mail.ru>
2022-08-31 22:26:42 +0400

    homework lesson7

--- lesson4-spring-boot/src/main/java/ru/lenivtsev/controller/ProductController.java
+++ lesson4-spring-boot/src/main/java/ru/lenivtsev/controller/ProductController.java
@@ -7,30 +7,51 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ru.lenivtsev.EntityNotFoundException;
+import ru.lenivtsev.exceptions.EntityNotFoundException;
 import ru.lenivtsev.products.Product;
 import ru.lenivtsev.products.ProductRepository;
 
 import javax.validation.Valid;
+import java.math.BigDecimal;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 @Controller
 @RequestMapping("/product")
 @RequiredArgsConstructor
 public class ProductController {
+        private static final Pattern PARAM_PATTERN_LESS = Pattern.compile("\\<(\\d+)");
+        private static final Pattern PARAM_PATTERN_MORE= Pattern.compile("\\>(\\d+)");
+        private static final Pattern PARAM_PATTERN_BETWEEN = Pattern.compile("(\\d+)\\-(\\d+)");
 
         private final ProductRepository productRepository;
 
         @GetMapping
-        public String listPage(Model model) {
-            model.addAttribute("products", productRepository.findAll());
+        public String listPage(@RequestParam Optional<String> productFilter, Model model) {
+            if (productFilter.isEmpty() || productFilter.get().isBlank()){
+                model.addAttribute("products", productRepository.findAll());
+
+            } else {
+                Matcher matcher = PARAM_PATTERN_BETWEEN.matcher(productFilter.get());
+                if (matcher.matches()) {
+                    model.addAttribute("products", productRepository.getProductsByCostBetween(new BigDecimal(matcher.group(1)), new BigDecimal(matcher.group(2))));
+                }
+                matcher = PARAM_PATTERN_LESS.matcher(productFilter.get());
+                if (matcher.matches()) {
+                    model.addAttribute("products", productRepository.getProductsByCostBefore(new BigDecimal(matcher.group(1))));
+                }
+                matcher = PARAM_PATTERN_MORE.matcher(productFilter.get());
+                if (matcher.matches()) {
+                    model.addAttribute("products", productRepository.getProductsByCostAfter(new BigDecimal(matcher.group(1))));
+                }
+            }
             return "product";
         }
 
         @GetMapping("/{id}")
         public String form(@PathVariable("id") long id, Model model) {
-            Optional<Product> optionalProduct = productRepository.findById(id);
             model.addAttribute("product", productRepository.findById(id)
                     .orElseThrow(() -> new EntityNotFoundException("Product not found")));
             return "product_form";
@@ -38,18 +59,18 @@ public class ProductController {
 
         @GetMapping("/new")
         public String addNewProduct(Model model) {
-            model.addAttribute("product", new Product("", 0L));
+            model.addAttribute("product", new Product("", BigDecimal.ZERO));
             return "product_form";
         }
 
         @DeleteMapping("{id}")
         public String deleteProductById(@PathVariable long id) {
-            productRepository.delete(id);
+            productRepository.deleteById(id);
             return "redirect:/product";
         }
 
         @PostMapping
-        public String saveProduct(@RequestBody @Valid Product product, BindingResult bindingResult) {
+        public String saveProduct(@Valid Product product, BindingResult bindingResult) {
             if (bindingResult.hasErrors()) {
                 return "product_form";
             }
--- lesson4-spring-boot/src/main/java/ru/lenivtsev/products/ProductRepository.java
+++ lesson4-spring-boot/src/main/java/ru/lenivtsev/products/ProductRepository.java
@@ -1,54 +1,24 @@
 package ru.lenivtsev.products;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
+import java.math.BigDecimal;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
+
 
 @Repository
-public class ProductRepository{
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    private final Map<Long, Product> productMap = new ConcurrentHashMap<>();
-    private final AtomicLong identity = new AtomicLong(0);
+    List<Product> getProductsByCostAfter(BigDecimal costFilter);
+    List<Product> getProductsByCostBefore(BigDecimal costFilter);
+    List<Product> getProductsByCostBetween(BigDecimal costFilterMin, BigDecimal costFilterMax);
 
-    @PostConstruct
-    public void init(){
-        this.insert(new Product("Potato", 5L));
-        this.insert(new Product("Tomato", 23L));
-        this.insert(new Product("Apple", 20L));
-        this.insert(new Product("Bananas", 10L));
-        this.insert(new Product("Carrot", 8L));
-    }
+    @Query(value = """
+        select * from products p 
+        where (:costFilter is null or p.cost > :costfilter)
+        """, nativeQuery = true)
+    List<Product> productByCost(BigDecimal costFilter);
 
-    public void insert(Product product){
-        long id = identity.incrementAndGet();
-        product.setId(id);
-        productMap.put(id, product);
-    }
-
-    public List<Product> findAll(){
-        return new ArrayList<>(productMap.values());
-    }
-
-    public Optional<Product> findById(long id) {
-
-        return Optional.ofNullable(productMap.get(id));
-    }
-
-    public void delete(long id) {
-        productMap.remove(id);
-    }
-
-    public Product save(Product product) {
-        if (product.getId() == null) {
-            product.setId(identity.incrementAndGet());
-        }
-        productMap.put(product.getId(), product);
-        return product;
-    }
 }

d768dce6e9ff09ce4dfc316bdee8b6f3b11f18c0
EvgeniLen <evgenilen@mail.ru>
2022-09-03 19:23:53 +0400

    homework lesson8 сделал переход по страницам, и скрытие кнопок если нет следующей\предыдущей страницы.

--- lesson4-spring-boot/src/main/java/ru/lenivtsev/products/ProductRepository.java
+++ lesson4-spring-boot/src/main/java/ru/lenivtsev/products/ProductRepository.java
@@ -1,8 +1,11 @@
 package ru.lenivtsev.products;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import ru.lenivtsev.model.Product;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -11,14 +14,21 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    List<Product> getProductsByCostAfter(BigDecimal costFilter);
-    List<Product> getProductsByCostBefore(BigDecimal costFilter);
-    List<Product> getProductsByCostBetween(BigDecimal costFilterMin, BigDecimal costFilterMax);
-
     @Query(value = """
-        select * from products p 
-        where (:costFilter is null or p.cost > :costfilter)
-        """, nativeQuery = true)
-    List<Product> productByCost(BigDecimal costFilter);
+            select * from products p 
+            where (:productTitleFilter is null or p.title like :productTitleFilter)
+            and ((:costFilterMin is null or :costFilterMax is null) or p.cost between :costFilterMin and :costFilterMax)
+            and (:costFilterMax is null or p.cost < :costFilterMax)
+            and (:costFilterMin is null or p.cost > :costFilterMin)
+            """,
+            countQuery = """
+            select count(*) from products p 
+            where (:productTitleFilter is null or p.title like :productTitleFilter)
+            and ((:costFilterMin is null or :costFilterMax is null) or p.cost between :costFilterMin and :costFilterMax)
+            and (:costFilterMax is null or p.cost < :costFilterMax)
+            and (:costFilterMin is null or p.cost > :costFilterMin)
+            """ ,
+            nativeQuery = true)
+    Page<Product> productByFilter(String productTitleFilter, BigDecimal costFilterMin, BigDecimal costFilterMax, Pageable pageable);
 
 }
---
+++ lesson4-spring-boot/src/main/java/ru/lenivtsev/service/ProductService.java
@@ -0,0 +1,51 @@
+package ru.lenivtsev.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import ru.lenivtsev.model.dto.ProductDto;
+import ru.lenivtsev.model.mapper.ProductDtoMapper;
+import ru.lenivtsev.products.ProductRepository;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private static final Pattern PARAM_PATTERN_BETWEEN = Pattern.compile("(\\d+)?\\-(\\d+)?");
+    private final ProductRepository productRepository;
+    private final ProductDtoMapper mapper;
+
+    public Page<ProductDto> findAllByFilter(String productTitleFilter, String costFilter, int page, int size){
+        productTitleFilter = productTitleFilter == null || productTitleFilter.isBlank() ? null : "%" + productTitleFilter.trim() + "%";
+        costFilter = costFilter == null || costFilter.isBlank() ? null : costFilter.trim();
+        BigDecimal costFilterMin = null;
+        BigDecimal costFilterMax = null;
+        if (costFilter!=null){
+            Matcher matcher = PARAM_PATTERN_BETWEEN.matcher(costFilter);
+
+            if (matcher.matches()) {
+                costFilterMin = matcher.group(1)==null ? null : new BigDecimal(matcher.group(1));
+                costFilterMax = matcher.group(2)==null ? null : new BigDecimal(matcher.group(2));
+            }
+        }
+        return productRepository.productByFilter(productTitleFilter, costFilterMin, costFilterMax, PageRequest.of(page, size))
+                .map(mapper::map);
+    }
+
+    public Optional<ProductDto> findByProductId(Long id) {
+        return productRepository.findById(id).map(mapper::map);
+    }
+
+    public void save(ProductDto productDto) {
+        productRepository.saveAndFlush(mapper.map(productDto));
+    }
+
+    public void deleteProductById(Long id){
+        productRepository.deleteById(id);
+    }
+}
